f9e4a4f454b701bb878b098bd43b828eb68100fd
Timur Kharin <timurx404@gmail.com>
2023-07-25 09:13:29 +0300

    Initial Commit

---
+++ Aqua.Recovery Watch App/Aqua_RecoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aqua_RecoveryApp.swift
+//  Aqua.Recovery Watch App
+//
+//  Created by Timur Kharin on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Aqua_Recovery_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
